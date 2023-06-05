f1482bd9bcf76a6ee039d5ec5d3305b652fc7fe5
Replit user <>
2023-06-05 20:37:47 +0000

    updates

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -9,6 +9,21 @@ day - day of the week
 */
 
 
+
+
+function coffeeTemplate(coffee) {
+return ` <p>
+    		 <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+            <strong class="feature">${coffee.day} Coffee Special:</strong> ${coffee.day} daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+
+  `;
+
+
+
+
+}
+
+
 //alert("Can you see this?");
 
 let myDate = new Date();
@@ -17,29 +32,35 @@ let today = myDate.getDay();
 
 //today = 3;
 
-switch(today){
+switch (today) {
 
- 	case 2:
-        	today = "Tuesday";
- 	break;
+  case 1:
+    today = "Monday"
+    coffee = {
+      name: "Bubble Tea",
+      color: "pink",
+      pic: "images/bubble-tea.jpg",
+      alt: "A picture of a bubble tea",
+      day: "Wednesday",
+      desc: `I like me some bubble tea`
+    };
+    break;
 
-     case 3:
-        today = "Wednesday";
- break;
+  case 2:
+    today = "Tuesday";
+    break;
 
- 	default:
-        	today ="Not sure what day it is!";
+  case 3:
+    today = "Wednesday";
+    break;
+
+  default:
+    today = "Not sure what day it is!";
 
 }
 
-coffee = {
-    name: "Bubble Tea",
-    color: "pink",
-    pic: "images/bubble-tea.jpg",
-    alt: "A picture of a bubble tea",
-    day: "Wednesday",
-    desc: `I like me some bubble tea`
-}
+
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 
 //alert(today);
 console.log(coffee);
